9dcbe28d0d8c75ade02858601d6748922de4fdb2
@eng.abdul.rahman19992 <eng.abdul.rahman19992@gmail.com>
2022-09-27 02:35:42 +0300

    Create template-start-coding.cpp

---
+++ template-start-coding.cpp
@@ -0,0 +1,63 @@
+/*All praise be to Allah*/
+#include <bits/stdc++.h>
+using namespace std;
+#define F first
+#define S second
+#define ii pair < int , int >
+#define LS pair < ll , string >
+#define FAST ios::sync_with_stdio(0);cin.tie(0);cout.tie(0);
+
+typedef unsigned long long ull;
+typedef unsigned int long long uill;
+typedef int long long ill;
+typedef long double ldbl;
+typedef long long ll;
+typedef double dbl;
+
+const ldbl E = 2.71828182845904523536;
+const ldbl pi = acos(-1);
+const dbl eps = 1e-9;
+const ll mod = 1e9+7;
+const ll oo = 1LL<<61;
+
+/* ///// ** function ** ///// */
+
+void out(ll a[],ll n)
+{
+    for(int i=0; i<n; i++)
+        cout<<a[i]<<" ";
+    cout<<"\n";
+
+}
+
+void out(vector<ll> v)
+{
+    for(auto a:v )
+        cout<<a<<"  ";
+    cout<<"\n";
+
+}
+
+/* ///// ** global declare ** ///// */
+#define N 500000
+ll a[N];
+
+void solver()
+{
+/* ///// ** code ** ///// */
+
+    printf("%lld\n",ans);
+}
+
+int main()
+{
+
+    ll t;
+    scanf("%lld",&t);
+    while(t--)
+    {
+        solver();
+    }
+
+    return 0;
+}
